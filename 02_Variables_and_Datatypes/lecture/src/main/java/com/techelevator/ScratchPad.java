504b37fd7ebe56922865423b5cf3a94295377111
Katherine <85465013+Katherine928@users.noreply.github.com>
2022-03-08 15:57:43 -0500

    Add files via upload

---
+++ 02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/ScratchPad.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+public class ScratchPad {
+    public static void main(String[] args) {
+
+        // declare a variable
+        int age = 14;
+        age = 17;
+        System.out.println("Hello Katherine! " + "age is: " + age);
+    }
+}
